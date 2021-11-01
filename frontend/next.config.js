c6df7cce3eaf06b8bb8c821f6527e3102c6e4799
mingyeongho <1736s@naver.com>
2021-11-01 21:29:39 +0900

    install modules(recoil, react@18, react-dom@18)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
-}
+
+  // swc 사용
+  swcMinify: true,
+
+  // Suspense
+  experimental: {
+    concurrentFeatures: true,
+  },
+
+  // AVIF
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+};
