bed8681233e26f3e89259abd762c1097099a3a4c
mingyeongho <1736s@naver.com>
2021-11-06 15:57:24 +0900

    card component

---
+++ frontend/components/Reusable/t_fx_card.jsx
@@ -0,0 +1,14 @@
+import style from "../../styles/t_fx_comp.module.scss";
+
+export default function t_fx_card({ children }) {
+  return (
+    <>
+      <div className={style.comp_card}>{children}</div>
+      {/* <style jsx>{`
+        div {
+          padding: 1rem;
+        }
+      `}</style> */}
+    </>
+  );
+}
