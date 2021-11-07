538ae36a08566df02ed0e6ccf8f141e1246edcb2
mingyeongho <1736s@naver.com>
2021-11-08 00:48:18 +0900

    html의 Head역할

---
+++ frontend/components/Common/HeadInfo.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const HeadInfo = () => {
+  return (
+    <div>
+      <Head>
+        <title>fc</title>
+        <meta name="description" content="fc" />
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default HeadInfo;
