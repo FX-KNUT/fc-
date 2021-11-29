b2e19adc39b9e98f38da9c32acc888e47ade1a8c
YongSiHun <tlgns1537@naver.com>
2021-11-30 01:47:46 +0900

    docker file and compose file, setup shell file create

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# backend and frontend docker container image build
+docker build --tag 2mukee/fc-backend:1.0 ./backend
+docker build --tag 2mukee/fc-frontend:1.0 ./frontend
+
+# upload to docker registry
+docker push 2mukee/fc-backend:1.0
+docker push 2mukee/fc-frontend:1.0
+
+# docker stack init (docker swarm)
+docker stack deploy -c ./docker-compose.yml fc-stack
