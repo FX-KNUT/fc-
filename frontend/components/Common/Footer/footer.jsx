70d5b19bfbbfe0a858e316d9f4e5a33fa18751a3
Lee <znlspf123@naver.com>
2021-12-02 17:42:34 +0900

    frontend code reviewed - code convention relavant

--- frontend/components/Common/Footer/footer.jsx
+++ frontend/components/Common/Footer/footer.jsx
@@ -3,14 +3,26 @@ import Facebook from "../../../public/ic_facebook.svg";
 import Kakao from "../../../public/ic_kakao.svg";
 import Instagram from "../../../public/ic_instagram.svg";
 
+const TEXT__CONTACT = "Contact us: 0000-0000"
+const TEXT__ADDRESS = "충청북도 충주시 대학로 50 한국교통대학교 중앙정보관"
+const TEXT__COPYRIGHT = "COPYRIGHT 2021 Korea National University of Transportation ALL RIGHTS RESERVED"
+
 const footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_wrapper}>
         <div className={styles.contact_wrapper}>
           <div className={styles.contact}>
-            <div>Contact us: 0000-0000</div>
-            <div>충청북도 충주시 대학로 50 한국교통대학교 중앙정보관</div>
+            <div>
+              <span>
+                {TEXT__CONTACT}
+              </span>
+            </div>
+            <div>
+              <span>
+                {TEXT__ADDRESS}
+              </span>
+            </div>
           </div>
           <div className={styles.sns_wrapper}>
             <div>
@@ -26,8 +38,9 @@ const footer = () => {
         </div>
         <hr />
         <div className={styles.copyright}>
-          COPYRIGHT 2021 Korea National University of Transpoertation ALL RIGHTS
-          RESERVED
+          <span>
+            {TEXT__COPYRIGHT}
+          </span>
         </div>
       </div>
     </footer>
