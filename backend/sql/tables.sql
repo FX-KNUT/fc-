d20aa32d3174ec7ef64df17afd09c3b491cc879a
bya2 <byaa1972@gmail.com>
2021-12-03 14:21:40 +0900

    Modify column 'user_id' in table, write ranking table, add foreign key in DDL

--- backend/sql/tables.sql
+++ backend/sql/tables.sql
@@ -1,6 +1,6 @@
 -- USER
 
--- @신이종
+-- @신이종 1203
 -- USER 테이블: 필요한 필드 추가 기재.
 -- 그 외 테이블 생성 SQL 작성.
 
@@ -15,18 +15,26 @@ CREATE TABLE user (
 
 
 -- MESSAGE
--- -- POSTs
--- -- NEWS_THUMBNAILs
+-- -- POST
+-- -- NEWS_THUMBNAIL
 -- -- COMMMENTs
--- -- REPORTs
+-- -- REPORT
 
 -- @이진형
 -- MESSAGE 테이블을 생성하고 POST 테이블과 조인하는 방법에 대해서 상의 후 결정.
 -- 테이블 필드 이름 앞에 엔티티명을 붙이는 것에 대해서 상의 후 결정.
 
+-- @신이종 1203 (1)
+-- message_user_id => user_id - 외래키이기 때문에 열 이름을 통일, COMMENTS 테이블도 이와 같음.
+-- POST 테이블은 이대로 사용. 모든 게시물을 POST 테이블에 담고 TARGET을 통해서 분류하고 DML.
+
+-- @신이종 1203 (2)
+-- 테이블 이름 뒤에 s를 안붙이려고 하는데, MYSQL에는 COMMENT라는 키워드가 따로 있기 때문에 이에 대해서 상의 필요.
+-- -- [ 테이블 명 뒤에 's'를 붙일 지, 다른 테이블명을 사용할지 ]
+
 CREATE TABLE posts (
   message_id INT(20),
-  message_user_id VARCHAR(15),
+  user_id VARCHAR(15),
   message_content VARCHAR(500),
   message_target VARCHAR(30),
   message_created_at TIMESTAMP,
@@ -34,18 +42,20 @@ CREATE TABLE posts (
   post_view_count INT(3),
   post_like_count INT(3),
   post_updated_at TIMESTAMP,
-  PRIMARY KEY(message_id)
+  PRIMARY KEY(message_id),
+  FORIEGN KEY(message_user_id) REFERENCES user (id)
 );
 
 CREATE TABLE news_thumbnails (
   message_id INT(20),
   news_thumbnail VARCHAR(100),
-  PRIMARY KEY(message_id)
+  PRIMARY KEY(message_id),
+  FOREIGN KEY(message_id) REFERENCES post (message_id)
 );
 
 CREATE TABLE comments (
   message_id INT(20),
-  message_user_id VARCHAR(15),
+  user_id VARCHAR(15),
   message_content VARCHAR(500),
   message_target VARCHAR(30),
   message_created_at TIMESTAMP,
@@ -54,38 +64,15 @@ CREATE TABLE comments (
   PRIMARY KEY(message_id)
 );
 
-CREATE TABLE board (
+-- @신이종 1203
+-- BOARD 테이블은 필요가 없다고 판단하므로 삭제.
+-- Message_target이 문자열이라서 게시판 생성 등의 로직이 없다면 쓸 이유가 없음
 
-);
+-- RANKING
 
-type Message struct {
-	Message_ID        uint      `json:"message_id"`
-	Message_UserID    uint      `json:"message_user_id" binding:"require"`
-	Message_Content   string    `json:"message_content" binding:"require"`
-	Message_Target    string    `json:"message_target"  binding:"require"`
-	Message_CreatedAt time.Time `json:"message_created_at"`
-}
-
-type Post struct { // 공지사항, FAQ, QnA, 커뮤니티
-	Message
-	Post_Title     string    `json:"post_title" 		 binding:"require"`
-	Post_ViewCount uint      `json:"post_view_count" binding:"require"`
-	Post_LikeCount uint      `json:"post_like_count" binding:"require"`
-	Post_UpdatedAt time.Time `json:"post_updated_at"`
-}
-
-type News struct { // 뉴스 (JOIN: Post + NewsPostThumbnail)
-	Post
-	News_Thumbnail string `json:"news_thumbnail"`
-}
-
-type Comment struct { // 댓글
-	Message
-	Comment_LikeCount uint `json:"comment_like_count" binding:"require"`
-	Comment_UpdatedAt time.Time `json:"comment_updated_at"`
-}
-
-type Report struct { // 신고
-	Message
-	Report_Reason uint `json:"report_reason" binding:"require"`
-}
+CREATE TABLE ranking (
+  user_id VARCHAR(15),
+  ranking_idx INT(3),
+  PRIMARY KEY(user_id),
+  FOREIGN KEY(user_id) REFERENCES user (id)
+)
