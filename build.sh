c57e11132c72d5e451720af10eb30fee41390ed7
YongSiHun <tlgns1537@naver.com>
2021-12-15 10:19:53 +0900

    jenkins publish over ssh setting

--- build.sh
+++ build.sh
@@ -6,10 +6,9 @@ bash /home/ec2-user/jenkins/workspace/fc-login.sh
 # backend and frontend docker container image build
 docker build --tag 2mukee/fc-backend:1.0 ./backend
 docker build --tag 2mukee/fc-frontend:1.0 ./frontend
+docker build --tag 2mukee/fc-build-server:1.0 ./build_server
 
 # upload to docker registry
 docker push 2mukee/fc-backend:1.0
 docker push 2mukee/fc-frontend:1.0
-
-# docker stack init (docker swarm)
-docker stack deploy -c ./docker-compose.yml fc-stack
+docker push 2mukee/fc-build-server:1.0
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# docker stack init (docker swarm)
+docker stack deploy -c ./docker-compose.yml fc-stack
