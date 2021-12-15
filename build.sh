6c28b4f68427e6b7be16835156a0fd251bdbbfff
YongSiHun <tlgns1537@naver.com>
2021-12-15 10:59:26 +0900

    build server fc-login path modified

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # dockerhub login
-bash /var/jenkins_home/fc-login.sh
+bash /usr/fc-login.sh
 
 # backend and frontend docker container image build
 docker build --tag 2mukee/fc-backend:1.0 ./backend
 docker build --tag 2mukee/fc-frontend:1.0 ./frontend
-docker build --tag 2mukee/fc-build-server:1.0 ./build_server
 
 # upload to docker registry
 docker push 2mukee/fc-backend:1.0
 docker push 2mukee/fc-frontend:1.0
-docker push 2mukee/fc-build-server:1.0
