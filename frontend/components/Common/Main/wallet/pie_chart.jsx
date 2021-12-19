844a01f89ee6ac6e3a583b6c158cc0f0479c10ec
mingyeongho <1736s@naver.com>
2021-12-20 00:35:56 +0900

    install pie chart

---
+++ frontend/components/Common/Main/wallet/pie_chart.jsx
@@ -0,0 +1,29 @@
+import styles from "../../../../styles/main/wallet/_wallet.module.scss";
+import Chart from "react-google-charts";
+
+const dummy_wallet = [
+  ["Coin", "Per"],
+  ["Wendy", 10],
+  ["Winter", 5],
+  ["IU", 22],
+];
+
+const pie_chart = () => {
+  return (
+    <div className={styles.wallet_wrapper}>
+      <Chart
+        width={"500px"}
+        height={"300px"}
+        chartType="PieChart"
+        loader={<div>Loading Your Wallet</div>}
+        data={dummy_wallet}
+        options={{
+          title: "Your Wallet",
+        }}
+        rootProps={{ "data-testid": "1" }}
+      />
+    </div>
+  );
+};
+
+export default pie_chart;
--- frontend/pages/wallet.js
+++ frontend/pages/wallet.js
@@ -1,7 +1,10 @@
+import Pie_chart from "../components/Common/Main/wallet/pie_chart";
+
 const wallet = () => {
   return (
     <div>
       <span>wallet</span>
+      <Pie_chart></Pie_chart>
     </div>
   );
 };
