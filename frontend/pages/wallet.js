175e777f1b1c5a92a89a13fb99e93f73158f09fd
Lee <znlspf123@naver.com>
2021-12-29 20:41:25 +0900

    wrap wallet_container inside Container(Reusable Component)

--- frontend/pages/wallet.js
+++ frontend/pages/wallet.js
@@ -1,10 +1,15 @@
 import Pie_chart from "../components/Common/Main/wallet/pie_chart";
+import Container from "../components/Reusable/t_fx__container";
+
+const TITLE_WALLET = "Wallet"
 
 const wallet = () => {
   return (
-    <div id="wallet_container">
-      <Pie_chart></Pie_chart>
-    </div>
+    <Container _str_title={TITLE_WALLET}>
+      <div id="wallet_container">
+        <Pie_chart></Pie_chart>
+      </div>
+    </Container>
   );
 };
 
