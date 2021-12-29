e7bb1c3707be76ddfb77fe98fa75f772c0b55496
Lee <znlspf123@naver.com>
2021-12-29 20:47:08 +0900

    Reusable Component Modified - t_fx__container.jsx

--- frontend/pages/wallet.js
+++ frontend/pages/wallet.js
@@ -5,7 +5,7 @@ const TITLE_WALLET = "Wallet"
 
 const wallet = () => {
   return (
-    <Container _str_title={TITLE_WALLET}>
+    <Container _str_title={TITLE_WALLET} _is_show_more={false}>
       <div id="wallet_container">
         <Pie_chart></Pie_chart>
       </div>
