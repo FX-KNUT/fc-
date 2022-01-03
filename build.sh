2f9bb5355844478740e8cc2511d881dabcddba89
YongSiHun <tlgns1537@naver.com>
2022-01-03 23:33:31 +0900

    Database container add

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ bash /var/lib/jenkins/workspace/mh-login.sh
 # backend and frontend docker container image build
 docker build --tag 2mukee/fc-backend:1.0 ./backend
 docker build --tag 2mukee/fc-frontend:1.0 ./frontend
+docker build --tag 2mukee/fc-database:1.0 .
 
 # upload to docker registry
 docker push 2mukee/fc-backend:1.0
 docker push 2mukee/fc-frontend:1.0
+docker push 2mukee/fc-database:1.0
