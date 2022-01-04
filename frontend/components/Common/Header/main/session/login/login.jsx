ec5606f3ce3b9b5349fe75224110a3b8cb45de5a
bya2 <byaa1972@gmail.com>
2022-01-04 21:45:34 +0900

    fix login component codes

--- frontend/components/Common/Header/main/session/login/login.jsx
+++ frontend/components/Common/Header/main/session/login/login.jsx
@@ -1,20 +1,7 @@
-import Link from "next/link";
 import { useState } from "react";
 import Modal from "../../../../../Reusable/t_fx__modal";
 import styles from "../../../../../../styles/header/main/session/login/_header_main_session_login.module.scss";
-import Logo from "../../logo/logo";
-import axios from "axios";
-import fn_hashing from "../../../../../Logic/fn_hashing";
-import Ic_login from "../../../../../../public/ic_login.svg";
 import Login_form from "./login_form";
-
-const login_header_components = [
-  {
-    alt: "logo",
-    class_name: styles.login_logo_wrapper,
-    component: <Logo />,
-  },
-];
 
 const Login = () => {
   // state
