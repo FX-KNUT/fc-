18a23af573c6cede52ee487ffa73c395798d047d
bya2 <byaa1972@gmail.com>
2022-01-08 21:26:53 +0900

    Change modal event to improve discomfort

--- frontend/components/Reusable/t_fx__modal.jsx
+++ frontend/components/Reusable/t_fx__modal.jsx
@@ -6,10 +6,17 @@ const T_fx__modal = ({ children, is_show__modal, fn_on_close }) => {
   return (
     is_show__modal &&
     ReactDOM.createPortal(
-      <div className={styles.comp_modal_wrapper} onClick={() => fn_on_close()}>
+      <div
+        className={styles.comp_modal_wrapper}
+        onMouseDown={(e) => {
+          e.target.addEventListener("mouseup", () => {
+            fn_on_close();
+          });
+        }}
+      >
         <div
           className={styles.comp_modal_content}
-          onClick={(e) => {
+          onMouseDown={(e) => {
             e.stopPropagation();
           }}
         >
